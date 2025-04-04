b0938fc6596ee233433811226c60de3f14cc098d
karam karam <116016222+Karamkaram17@users.noreply.github.com>
2025-04-04 18:41:31 +0300

    Update By Karam Karam

---
+++ data.js
@@ -0,0 +1,21 @@
+const SERVICE_DATA = {
+  "serviceId": "6758b167f1ce4d064076b895",
+  "data": {
+    "whatsapp_url": "https://wa.me/+96181406392",
+    "instagram_url": "https://www.instagram.com/zeinalahoud__mua/",
+    "service_3_title": "Special Events",
+    "service_3_description": "Stand out at any event with makeup that complements your style and personality.",
+    "price_list_img": "https://menus.blob.core.windows.net/shared/6758b167f1ce4d064076b895/26be2747-c0b3-43f6-a7ed-24dd756f9d9b.jpg",
+    "clients_1_img": "https://menus.blob.core.windows.net/shared/6758b167f1ce4d064076b895/7129de06-ceed-4444-8f9e-da6a11c0df8a.png",
+    "clients_2_img": "https://menus.blob.core.windows.net/shared/6758b167f1ce4d064076b895/065d3e4e-b9a5-45e3-bdf6-63895dbd5bd9.png",
+    "clients_3_img": "https://menus.blob.core.windows.net/shared/6758b167f1ce4d064076b895/de6f2868-7a5e-4b72-b639-2d09f10a1551.png",
+    "clients_4_img": "https://menus.blob.core.windows.net/shared/6758b167f1ce4d064076b895/be72728e-cb00-4768-9efc-95eb2df073e5.png",
+    "main_title": "Enhance Your Beauty",
+    "main_subtitle": "Exclusive Makeup Services by Zeina Lahoud",
+    "about_content": "I am Zeina Lahoud, a passionate makeup artist trained under Make Up Forever. With my certification and specialized training, I am dedicated to crafting the perfect look for your most memorable moments.",
+    "service_1_title": "Bridal Makeup",
+    "service_1_description": "Make your special day unforgettable with a bespoke bridal look.",
+    "service_2_title": "Photoshoot Makeup",
+    "service_2_description": "Ensure your photoshoot shines with flawless makeup tailored to your vision."
+  }
+};
